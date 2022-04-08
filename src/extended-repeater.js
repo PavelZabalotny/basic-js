7e6e4881e75bcd5bfe86fcbddc8d89e183a8d74a
PavelZabalotny <pavelzabalotny@gmail.com>
2022-04-08 19:27:39 +0300

    feat: implement extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
@@ -15,9 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, {
+  repeatTimes = 1,
+  separator = '+',
+  addition,
+  additionRepeatTimes = 1,
+  additionSeparator = '|',
+} = {}) {
+  str = String(str)
+  addition = addition !== undefined ? String(addition) : ''
+
+  const res = []
+  const additionalToAdd = []
+
+  if (addition) {
+    let addStr = [addition, additionSeparator]
+    for (let j = 1; j <= additionRepeatTimes; j++) {
+      additionalToAdd.push(...addStr)
+    }
+    additionalToAdd.splice(-1)
+  }
+
+  const strToAdd = [str, ...additionalToAdd, separator]
+
+  for (let i = 1; i <= repeatTimes; i++) {
+    res.push(...strToAdd)
+  }
+
+  res.splice(-1)
+
+  return res.join('')
 }
 
 module.exports = {
